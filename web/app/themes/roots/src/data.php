a87afe2218b81a1f54761536b3710af020c7acdd
Aaron Ranard <aaronranard@gmail.com>
2017-03-20 16:24:04 -0600

    menu data needs to be global

--- web/app/themes/roots/src/data.php
+++ web/app/themes/roots/src/data.php
@@ -1,13 +1,24 @@
 <?php
 namespace App;
 
-add_filter('sage/template/home/data', function() {
-    return [
-        'menu' => [
-            'professional'    => 'Professional',
-            'personal'    => 'Personal',
-            'blog'  => 'Blog',
-            'about' => 'About'
-        ]
-    ]; 
+// Layout Specific
+// add_filter('sage/template/home/data', function($data) {
+//     $data['menu'] = [
+//         'professional'    => 'Professional',
+//         'personal'    => 'Personal',
+//         'blog'  => 'Blog',
+//         'about' => 'About'
+//     ];
+//     return $data;
+// });
+
+add_action('after_setup_theme', function () {
+  // Globals
+  sage('blade')->share('menu', 
+    [
+        'professional'    => 'Professional',
+        'personal'    => 'Personal',
+        'blog'  => 'Blog',
+        'about' => 'About'
+    ]);
 });
